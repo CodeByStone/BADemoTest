755da60d76ade676217a03e8ef63413c3d0da48b
博爱1616 <boai@users.noreply.github.com>
2016-04-04 23:01:36 +0800

    Update ViewController.m

--- demoTest/ViewController.m
+++ demoTest/ViewController.m
@@ -16,8 +16,9 @@
 
 
 @property (nonatomic, strong) UITableView *tableView;
-@property (nonatomic, strong) NSMutableArray *titlesArray;
+@property (nonatomic, strong) NSArray *titlesArray;
 @property (nonatomic, strong) NSMutableArray *classNamesArray;
+@property (nonatomic, strong) NSDictionary *dict;
 
 @end
 
@@ -46,35 +47,61 @@
         
         _tableView.tableFooterView = [UIView new];
         
-        self.titlesArray = @[].mutableCopy;
-        self.classNamesArray = @[].mutableCopy;
+//        [self creatData];
         
-        [self addCell:@"1、自定义View(多重嵌套tableView)" class:@"DemoVC1"];
-        [self addCell:@"2、微信朋友圈" class:@"DemoVC2"];
-        [self addCell:@"3、自适应文字高度" class:@"DemoVC3"];
-        [self addCell:@"4、测试tableView" class:@"DemoVC4"];
-        [self addCell:@"5、评论界面(WHC)" class:@"DemoVC5"];
-        [self addCell:@"6、评论界面(SD)" class:@"DemoVC6"];
-        [self addCell:@"7、评论列表" class:@"DemoVC7"];
-        [self addCell:@"8、动态该表label的高度" class:@"DemoVC8"];
-        [self addCell:@"9、NSString扩展" class:@"DemoVC9"];
-        [self addCell:@"10、多级评论" class:@"DemoVC10"];
-        [self addCell:@"11、城市选择器(pickView)" class:@"DemoVC11"];
-        [self addCell:@"12、alertView封装" class:@"DemoVC12"];
-        [self addCell:@"13、文字和APP名字的国际化(webView封装)" class:@"DemoVC13"];
-        [self addCell:@"14、自定义collection" class:@"DemoVC14"];
-
-        
-        [self.tableView reloadData];
     }
     return _tableView;
 }
 
-- (void)addCell:(NSString *)title class:(NSString *)className
+- (NSArray *)titlesArray
 {
-    [self.titlesArray addObject:title];
-    [self.classNamesArray addObject:className];
+    if (!_titlesArray) {
+        _titlesArray = @[@"1、自定义View(多重嵌套tableView)", @"2、微信朋友圈", @"3、自适应文字高度", @"4、测试tableView", @"5、评论界面(WHC)", @"6、评论界面(SD)", @"7、评论列表", @"8、动态该表label的高度", @"9、NSString扩展",  @"10、多级评论", @"11、城市选择器(pickView)", @"12、alertView封装", @"13、文字和APP名字的国际化(webView封装)", @"14、自定义collection"];
+    }
+    return _titlesArray;
 }
+
+- (NSMutableArray *)classNamesArray
+{
+    if (!_classNamesArray) {
+        _classNamesArray = @[].mutableCopy;
+        for (int i = 0; i < self.titlesArray.count; i++)
+        {
+            NSString *name = [NSString stringWithFormat:@"DemoVC%d",i+1];
+            [_classNamesArray addObject:name];
+        }
+    }
+    return _classNamesArray;
+}
+
+//- (void)creatData
+//{
+//    self.titlesArray = @[].mutableCopy;
+//    self.classNamesArray = @[].mutableCopy;
+//
+//    [self addCell:@"1、自定义View(多重嵌套tableView)" class:@"DemoVC1"];
+//    [self addCell:@"2、微信朋友圈" class:@"DemoVC2"];
+//    [self addCell:@"3、自适应文字高度" class:@"DemoVC3"];
+//    [self addCell:@"4、测试tableView" class:@"DemoVC4"];
+//    [self addCell:@"5、评论界面(WHC)" class:@"DemoVC5"];
+//    [self addCell:@"6、评论界面(SD)" class:@"DemoVC6"];
+//    [self addCell:@"7、评论列表" class:@"DemoVC7"];
+//    [self addCell:@"8、动态该表label的高度" class:@"DemoVC8"];
+//    [self addCell:@"9、NSString扩展" class:@"DemoVC9"];
+//    [self addCell:@"10、多级评论" class:@"DemoVC10"];
+//    [self addCell:@"11、城市选择器(pickView)" class:@"DemoVC11"];
+//    [self addCell:@"12、alertView封装" class:@"DemoVC12"];
+//    [self addCell:@"13、文字和APP名字的国际化(webView封装)" class:@"DemoVC13"];
+//    [self addCell:@"14、自定义collection" class:@"DemoVC14"];
+//
+//    [self.tableView reloadData];
+//}
+
+//- (void)addCell:(NSString *)title class:(NSString *)className
+//{
+//    [self.titlesArray addObject:title];
+//    [self.classNamesArray addObject:className];
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
