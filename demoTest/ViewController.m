a66f399251a41f30fc79104869ebcb82e5d86b25
博爱 <sunboyan@outlook.com>
2016-04-09 17:47:37 +0800

    新增首页的樱花动画！

--- demoTest/ViewController.m
+++ demoTest/ViewController.m
@@ -124,13 +124,15 @@
     [super viewDidLoad];
     
 //    self.title = NSLocalizedString(@"title", nil);
-    self.title = BALocalizedString(@"title", @"博爱Demo测试");
+//    self.title = BALocalizedString(@"title", @"博爱Demo测试");
     self.title = BALocalizedString(@"title", nil);
     self.view.backgroundColor = [UIColor whiteColor];
     
     [BAFPSLabel showInWindow:[UIApplication sharedApplication].keyWindow];
     
     self.tableView.hidden = NO;
+    
+    [self starCoreAnimation];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -166,6 +168,57 @@
     }
     // 点击立刻取消该cell的选中状态
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (void)starCoreAnimation
+{
+    // =================== 樱花飘落 ====================
+    CAEmitterLayer * snowEmitterLayer = [CAEmitterLayer layer];
+    snowEmitterLayer.emitterPosition = CGPointMake(100, -30);
+    snowEmitterLayer.emitterSize = CGSizeMake(self.view.bounds.size.width * 2, 0);
+    snowEmitterLayer.emitterMode = kCAEmitterLayerOutline;
+    snowEmitterLayer.emitterShape = kCAEmitterLayerLine;
+    //    snowEmitterLayer.renderMode = kCAEmitterLayerAdditive;
+    
+    CAEmitterCell * snowCell = [CAEmitterCell emitterCell];
+    snowCell.contents = (__bridge id)[UIImage imageNamed:@"樱花瓣2"].CGImage;
+    
+    // 花瓣缩放比例
+    snowCell.scale = 0.02;
+    snowCell.scaleRange = 0.5;
+    
+    // 每秒产生的花瓣数量
+    snowCell.birthRate = 7;
+    snowCell.lifetime = 80;
+    
+    // 每秒花瓣变透明的速度
+    snowCell.alphaSpeed = -0.01;
+    
+    // 秒速“五”厘米～～
+    snowCell.velocity = 40;
+    snowCell.velocityRange = 60;
+    
+    // 花瓣掉落的角度范围
+    snowCell.emissionRange = M_PI;
+    
+    // 花瓣旋转的速度
+    snowCell.spin = M_PI_4;
+    
+    // 每个cell的颜色
+    //    snowCell.color = [[UIColor redColor] CGColor];
+    
+    // 阴影的 不透明 度
+    snowEmitterLayer.shadowOpacity = 1;
+    // 阴影化开的程度（就像墨水滴在宣纸上化开那样）
+    snowEmitterLayer.shadowRadius = 8;
+    // 阴影的偏移量
+    snowEmitterLayer.shadowOffset = CGSizeMake(3, 3);
+    // 阴影的颜色
+    snowEmitterLayer.shadowColor = [[UIColor whiteColor] CGColor];
+    
+    
+    snowEmitterLayer.emitterCells = [NSArray arrayWithObject:snowCell];
+    [self.tableView.backgroundView.layer addSublayer:snowEmitterLayer];
 }
 
 
