bff94391295d0101de6a323ac5ba799334c2d717
博爱“git config --global user.name 博爱” <sunboyan@outlook.com>
2016-04-27 16:38:14 +0800

    新增自适应tabelHeaderView！

--- demoTest/Demo/DemoVC25/DemoVC25.m
+++ demoTest/Demo/DemoVC25/DemoVC25.m
@@ -20,6 +20,9 @@
 @property (nonatomic, strong) NSArray         *titlesArray;
 //@property (nonatomic, strong) NSArray         *urlsArray;
 @property (nonatomic, strong) NSMutableArray  *dataArray;
+@property (nonatomic, strong) UIView          *headView;
+@property (strong, nonatomic) UILabel         *label;
+
 
 @end
 
@@ -41,11 +44,63 @@
         }];
         
         _tableView.tableFooterView = [UIView new];
-        
-        //        [self creatData];
-        
     }
     return _tableView;
+}
+
+- (UIView *)headView
+{
+    if (!_headView)
+    {
+        _headView = [UIView new];
+        _headView.backgroundColor = [UIColor yellowColor];
+        
+        UILabel *label = [UILabel new];
+        label.numberOfLines = 3;
+        label.text = @"iPhone SE 采用了备受欢迎的设计，并让它更加出色。铝金属表面经过喷砂工艺处理，打造出绸缎般的质感，让这款小巧、轻便的手机握持时手感非常舒适。绚丽的 4 英寸1 Retina 显示屏让图像显得生动、锐利。倒角边缘经过哑光处理，不锈钢 Apple 标志的颜色也与机身相配，整个外观显得格外精致。";
+        _label = label;
+        // 文字内容Frame
+//        CGFloat textX = _headView.x;
+//        CGFloat textY = _headView.top;
+//        CGFloat textW = BA_SCREEN_WIDTH - 2 * 10;
+//        CGRect textSize = [label.text boundingRectWithSize:CGSizeMake(textW, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:BA_FontSize(14)} context:nil];
+//        
+//        _label.frame = (CGRect){{textX,textY},textSize.size};
+        
+        // 按钮
+        UIButton *moreBtn = [UIButton new];
+        [moreBtn setTitle:@"全部" forState:UIControlStateNormal];
+        [moreBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [moreBtn addTarget:self action:@selector(moreBtnClick:) forControlEvents:UIControlEventTouchUpInside ];
+        moreBtn.selected = YES;
+        moreBtn.backgroundColor = BA_Red_Color;
+//        moreBtn.frame = CGRectMake(BA_SCREEN_WIDTH - 50, _label.bottom + 15, 40, 20);
+        
+        // 添加到headView
+        [_headView addSubview:moreBtn];
+        [_headView addSubview:label];
+
+//        _headView.frame = CGRectMake(0, 0, BA_SCREEN_WIDTH, moreBtn.bottom + 10);
+//        [_headView layoutIfNeeded]; // 调用这个方法之后有frame才能加入headView
+        
+        //////////////////////////////    开始布局  /////////////////////////////////////////
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.left.right.mas_equalTo(_headView);
+        }];
+        
+        [moreBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(label.mas_bottom).offset(10);
+            make.right.equalTo(label).offset(-10);
+        }];
+        
+        [_headView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(BA_SCREEN_WIDTH);
+            make.bottom.mas_equalTo(moreBtn).offset(10); // 实现动态调整headview的高度
+        }];
+        
+        [_headView layoutIfNeeded]; // 调用这个方法之后有frame才能加入headView
+    }
+    return _headView;
 }
 
 - (NSArray *)titlesArray
@@ -83,7 +138,8 @@
     [super viewDidLoad];
     
     self.tableView.hidden = NO;
-    
+    self.tableView.tableHeaderView = self.headView;
+
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -123,12 +179,19 @@
         [self.navigationController pushViewController:detailVC animated:YES];
     }
 
-    
-    
-    
     // 点击立刻取消该cell的选中状态
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
+/**
+ *  监听按钮点击切换显示的行数
+ */
+- (IBAction)moreBtnClick:(UIButton *)sender
+{
+    self.label.numberOfLines = self.label.numberOfLines == 0? 3:0;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.tableView.tableHeaderView = self.headView;
+    });
+}
 
 @end
